8701e2ec99f6ad0fdd4cf35cc66dcfe119da63d4
stefan3456 <stefan.bischof42@gmail.com>
2020-02-24 15:03:35 +0100

    buttons

--- app/src/main/java/a6br/quizapp3/MainActivity.java
+++ app/src/main/java/a6br/quizapp3/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+
+public class MainActivity extends Activity implements View.OnClickListener {
+    private Button beins;
+    private Button bzwei;
+    private Button bdrei;
+    private Button bvier;
+    private Button bfünf;
+
+    this.beins.setOnClickListener(this);
+    this.bzwei.setOnClickListener(this);
 }
