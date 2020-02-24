6e297ee86beebde25bbae4ab7294347ccc9396bf
teuflischernashornkrieger <david.pacher@gmx.net>
2020-02-24 15:11:18 +0100

    Definition Frage

---
+++ app/src/main/java/a6br/quizapp3/createquestion.java
@@ -0,0 +1,14 @@
+package a6br.quizapp3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class createquestion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_createquestion);
+    }
+}
---
+++ app/src/main/java/a6br/quizapp3/question.java
@@ -0,0 +1,11 @@
+package a6br.quizapp3;
+
+public class question {
+    private String questionText;
+    private String wrongAnswerOne;
+    private String wrongAnswerTwo;
+    private String wrongAnswerThree;
+    private String rightAnswer;
+    private String category;
+    }
+}
