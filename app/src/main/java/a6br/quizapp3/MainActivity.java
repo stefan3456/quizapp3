2efd36b63ee857cd912df275b59af88c57f77361
Jilaiyaa <calvinzenz@gmail.com>
2020-04-20 14:52:38 +0200

    restored main activity

--- app/src/main/java/a6br/quizapp3/MainActivity.java
+++ app/src/main/java/a6br/quizapp3/MainActivity.java
@@ -5,28 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button bone;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button bOne = findViewById(R.id.bOne);
-        Button bTwo = findViewById(R.id.bTwo);
-        Button bThree = findViewById(R.id.bThree);
-        Button bFour = findViewById(R.id.bFour);
+        this.bone = (Button) this.findViewById(R.id.bone);
+        this.bone.setOnClickListener(this);}
 
-        bOne.setOnClickListener(this);
-        bTwo.setOnClickListener(this);
-        bThree.setOnClickListener(this);
-        bFour.setOnClickListener(this);
-    }
+    //Intent intent = new Intent(this, AnotherActivity.class);
+    //startActivity(intent);
+
 
     @Override
     public void onClick(View view) {
 
+
     }
 }
+
 
